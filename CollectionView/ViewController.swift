c2888e6c29cc9574f6be03ef7be7c66d8f994c45
Yeliz Kececi <yelizkececi>
2020-11-29 20:30:56 +0300

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Yeliz Keçeci on 29.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
